0c4bd333acd97af033b828a296a14ba144189169
r0cky21 <121492370+r0cky21@users.noreply.github.com>
2023-01-27 23:34:03 +0530

    Add files via upload

---
+++ even&odd&appendfunc.py
@@ -0,0 +1,37 @@
+#even and odd number--
+
+n=float(input("enter number : "))
+
+if (n%2==0):
+   print("even number")
+
+else:
+    print("odd number")
+
+even = []
+odd = []
+
+
+
+
+
+
+
+
+#append--
+n=float(input("enter number : "))
+
+if (n%2==0):
+   print("even number")
+
+else:
+    print("odd number")
+
+even = []
+odd = []
+
+for n in range(1,101):
+    if (n%2==0):
+        even.append(n)
+    else:
+        odd.append(n)
